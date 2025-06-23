67bb5c06b79f3e6a05ae3d345a8bf4ec4161680c
estelacode <estelamadariagalasala@gmail.com>
2025-06-23 14:43:52 +0200

    Refactor main file

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 
 import gradio as gr
-import pymupdf
-
-def load_pdf(pdf_file:str)->str:
-    
-    """
-    This function takes a PDF file and extracts all its text.
-    
-    Parameters:
-    pdf_file (str): Path to the PDF file.
-    
-    Returns:
-    str: The extracted text.
-    """
-    print(pdf_file)
-    doc = pymupdf.open(pdf_file)
-
-    text = ""
-    for page in doc:
-        text += str(page.get_text())
-        print(f"Page {page.number}: {page.get_text().encode('utf-8')}") 
-    return text
+from llama_parse_lab.utils import load_pdf_pymupdf, load_pdf_llama_parse
 
 
-demo = gr.Interface(fn = load_pdf, inputs = gr.File(file_types=[".pdf"]), outputs = gr.Text())
+
+demo = gr.Interface(fn = load_pdf_llama_parse, inputs = gr.File(file_types=[".pdf"]), outputs = gr.Text())
 demo.launch()
