0e7b05a82a785aa8044c5c2ab97106022a26d5ca
estelacode <estelamadariagalasala@gmail.com>
2025-06-23 14:42:18 +0200

    Feature: Add utils module

---
+++ src/llama_parse_lab/utils.py
@@ -0,0 +1,67 @@
+import pymupdf
+from llama_cloud_services import LlamaParse
+import os
+
+def load_pdf_pymupdf(pdf_file:str)->str:
+    
+    """
+    This function takes a PDF file and extracts all its text.
+    
+    Parameters:
+    pdf_file (str): Path to the PDF file.
+    
+    Returns:
+    str: The extracted text.
+    """
+    print(pdf_file)
+    doc = pymupdf.open(pdf_file)
+
+    text = ""
+    for page in doc:
+        text += str(page.get_text())
+        print(f"Page {page.number}: {page.get_text().encode('utf-8')}") 
+    return text
+
+
+
+def load_pdf_llama_parse(pdf_file:str)->str:
+    
+    """
+    This function extracts text from a PDF file using the LlamaParse service.
+
+    Parameters:
+    pdf_file (str): Path to the PDF file.
+
+    Returns:
+    str: The extracted text from the PDF file.
+
+    Raises:
+    ValueError: If the LLAMA_CLOUD_API_KEY environment variable is not set.
+
+    Note:
+    - The function requires a valid API key for the LlamaParse service to be set 
+      in the LLAMA_CLOUD_API_KEY environment variable.
+    - The text from the PDF is returned as a single document.
+    """
+    
+     # Check if the environment variable is set
+    if not os.environ.get("LLAMA_CLOUD_API_KEY"):
+        raise ValueError("LLAMA_CLOUD_API_KEY is not set")
+    api_key = os.environ.get("LLAMA_CLOUD_API_KEY")
+    
+    # Create an instance of the LlamaParse class
+    parser = LlamaParse(api_key=api_key, num_workers=1,verbose=True,language="en")
+
+    # Parse the PDF file
+    job_result = parser.parse(pdf_file)
+
+    # Get the text from the job result in a single document
+    text_documents = job_result.get_text_documents(split_by_page=False)
+
+    # Get the text from the first document
+    text = text_documents[0].text
+
+    return text
+
+
+  
